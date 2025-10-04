97f99b0fbd85c79522ba1c36325356c3061e9026
Mihail <mihailsorin58@gmail.com>
2025-10-04 11:08:14 +0500

    Lab 3 Table

--- src/Lab1/table.php
+++ src/Lab1/table.php
@@ -1,3 +1,8 @@
+<?php 
+    $cols = 5;
+    $rows = 5;
+    
+?>
 <!DOCTYPE html>
 <html>
 
@@ -38,43 +43,24 @@
       <input type='submit' value='Создать' />
     </form>
     <!-- Таблица -->
-    <table border='1' width="200">
-      <tr>
-        <td>1</td>
-        <td>2</td>
-        <td>3</td>
-        <td>4</td>
-        <td>5</td>
-      </tr>
-      <tr>
-        <td>2</td>
-        <td>4</td>
-        <td>6</td>
-        <td>8</td>
-        <td>10</td>
-      </tr>
-      <tr>
-        <td>3</td>
-        <td>6</td>
-        <td>9</td>
-        <td>12</td>
-        <td>15</td>
-      </tr>
-      <tr>
-        <td>4</td>
-        <td>8</td>
-        <td>12</td>
-        <td>16</td>
-        <td>10</td>
-      </tr>
-      <tr>
-        <td>5</td>
-        <td>10</td>
-        <td>15</td>
-        <td>20</td>
-        <td>25</td>
-      </tr>
-    </table>
+    <?php
+echo "<table border='1' cellspacing='0' cellpadding='5'>";
+
+for ($r = 1; $r <= $rows; $r++) {
+    echo "<tr>";
+    for ($c = 1; $c <= $cols; $c++) {
+        $value = $r * $c;
+        if ($r == 1 || $c == 1) {
+            echo "<td style='font-weight: bold; text-align: center; background-color: #ddd;'>$value</td>";
+        } else {
+            echo "<td>$value</td>";
+        }
+    }
+    echo "</tr>";
+}
+
+echo "</table>";
+?>
     <!-- Таблица -->
     <!-- Область основного контента -->
   </div>
