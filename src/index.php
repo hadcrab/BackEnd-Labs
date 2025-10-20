62496857287bc894b78e2c1320123a07e5504d32
Mihail <mihailsorin58@gmail.com>
2025-10-20 11:04:10 +0500

    Change index.php

--- src/index.php
+++ src/index.php
@@ -1,3 +1,6 @@
-<h1> BackEnd Labs <h1>
-<?php echo "php test"; ?>
-
+<body>
+    <div style="display: flex; align-items: center;">
+         <h1> BackEnd Labs/Tests <h1><br>
+         <?php echo "php echo test"; ?>
+    </div>
+</body>
